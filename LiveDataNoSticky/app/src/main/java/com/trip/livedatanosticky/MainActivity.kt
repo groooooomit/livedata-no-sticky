96ba32fb22d136784eaddca03c38e1ead254be4b
fubo <fubofubo@hotmail.com>
2022-07-31 11:20:45 +0800

    完善 demo

--- LiveDataNoSticky/app/src/main/java/com/trip/livedatanosticky/MainActivity.kt
+++ LiveDataNoSticky/app/src/main/java/com/trip/livedatanosticky/MainActivity.kt
@@ -16,18 +16,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.btCount.setOnClickListener {
-            viewModel.increment()
-        }
+        /* 给一个初始文案 */
+        binding.txtData.text = "--"
+        binding.txtNoSticky.text = "--"
 
+        /* state 场景 */
         viewModel.data.observe(this) {
             binding.txtData.text = it?.toString()
         }
 
+        /* event 场景. */
         viewModel.data.observeNoSticky(this) {
             binding.txtNoSticky.text = it?.toString()
         }
 
+        /* 如果在 observe* 前 viewModel.data 有初始值，txtData 会收到粘性数据，txtNoSticky 不会. */
         /* 旋转屏幕后，activity 重建, txtData 会收到粘性数据，txtNoSticky 不会. */
+
+        /* 设置点击⌚️. */
+        binding.btCount.setOnClickListener {
+            viewModel.increment()
+        }
+
+
     }
 }
