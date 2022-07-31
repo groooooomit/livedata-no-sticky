bf3baeee1cb4cf695128dc70fefa19c1c4c6f933
fubo <fubofubo@hotmail.com>
2022-07-31 11:31:07 +0800

    完善 demo

--- LiveDataNoSticky/app/build.gradle.kts
+++ LiveDataNoSticky/app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation("com.google.android.material:material:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.lifecycle:lifecycle-viewmodel:2.5.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.5.1")
     implementation("androidx.activity:activity-ktx:1.5.1")
     implementation(project(":livedata-no-sticky"))
     testImplementation("junit:junit:4.13.2")
--- LiveDataNoSticky/app/src/main/java/com/trip/livedatanosticky/MainActivity.kt
+++ LiveDataNoSticky/app/src/main/java/com/trip/livedatanosticky/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.trip.livedatanosticky
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.map
 import com.bfu.livedata.nosticky.observeNoSticky
 import com.trip.livedatanosticky.databinding.ActivityMainBinding
 
@@ -12,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     private val viewModel by viewModels<MainViewModel>()
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -19,15 +22,33 @@ class MainActivity : AppCompatActivity() {
         /* 给一个初始文案 */
         binding.txtData.text = "--"
         binding.txtNoSticky.text = "--"
+        binding.txtData2.text = "--"
+        binding.txtNoSticky2.text = "--"
 
         /* state 场景 */
         viewModel.data.observe(this) {
-            binding.txtData.text = it?.toString()
+            binding.txtData.text = "源数据（state）：$it"
         }
 
         /* event 场景. */
         viewModel.data.observeNoSticky(this) {
-            binding.txtNoSticky.text = it?.toString()
+            binding.txtNoSticky.text = "源数据（event）：$it"
+        }
+
+        /* 因为对 LiveData 零入侵，所以 liveData 可以进行诸如 map、filter、flatmap 等各种变换. */
+        val data2 = viewModel.data.map {
+            /* 源数据乘以 2 */
+            it?.run { this * 2 }
+        }
+
+        /* state 场景 */
+        data2.observe(this) {
+            binding.txtData2.text = "源数据（state）* 2：$it"
+        }
+
+        /* event 场景. */
+        data2.observeNoSticky(this) {
+            binding.txtNoSticky2.text = "源数据（event）* 2：$it"
         }
 
         /* 如果在 observe* 前 viewModel.data 有初始值，txtData 会收到粘性数据，txtNoSticky 不会. */
